2fe3cffd818232068d8feec5765a6174f690ec07
undxcim <sosamauriciog@outlook.com>
2022-03-03 00:20:27 -0300

    prueba VSC

--- index.py
+++ index.py
@@ -10,5 +10,17 @@ def principal():
 def Grilla():
     return render_template('grilla.html')
 
+@app.route('/horariosdia1')
+def horariosdia1():
+    return render_template('horariosdia1.html')
+
+@app.route('/horariosdia2')
+def horariosdia2():
+    return render_template('horariosdia2.html')
+
+@app.route('/showkids')
+def showkids():
+    return render_template('showkids.html')
+
 if __name__ == '__main__':
     app.run(debug=True, port=2022)
